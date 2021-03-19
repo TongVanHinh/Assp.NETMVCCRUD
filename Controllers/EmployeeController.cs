bb12dad29ae31b78a3f789d3c61355ba73aa21bf
Tong Hinh <Tong Hinh@DESKTOP-CMJ018P>
2021-03-19 15:27:33 +0700

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assp.NETMVCCRUD.Models;
+
+namespace Assp.NETMVCCRUD.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetData()
+        {
+            using (DbModel db = new DbModel())
+            {
+                List<Employee> employees = db.Employees.ToList<Employee>();
+                return Json(new { data = employees }, JsonRequestBehavior.AllowGet);
+            }
+        }
+        //Employee/AddOrEdit
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new Employee());
+            else
+            {
+                using (DbModel db = new DbModel())
+                {
+                    return View(db.Employees.Where(x => x.EmployeeID == id).FirstOrDefault<Employee>());
+                }
+            }
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(Employee emp)
+        {
+            using (DbModel db = new DbModel())
+            {
+                if (emp.EmployeeID == 0)
+                {
+                    db.Employees.Add(emp);
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Created successfully" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    db.Entry(emp).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Edited successfully" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            using (DbModel db = new DbModel())
+            {
+                Employee emp = db.Employees.Where(x => x.EmployeeID == id).FirstOrDefault<Employee>();
+                db.Employees.Remove(emp);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Deleted successfully" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
